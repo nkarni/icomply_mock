221abcaf04bb2b2fa7357dd51050a99fc4e4e954
Nitzan Karni <nitzan.karni@gmail.com>
2022-01-31 10:29:54 +1100

    Change siet title in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'fmockup',
+    title: 'LSC Prototype',
     htmlAttrs: {
       lang: 'en'
     },
