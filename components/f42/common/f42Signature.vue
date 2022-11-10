181ecc08f667dd9956459181b16b063c4efcf652
nkarni <nk@karni.net.au>
2022-11-10 13:00:08 +1100

    f42

--- components/f42/common/f42Signature.vue
+++ components/f42/common/f42Signature.vue
@@ -6,31 +6,13 @@
       <h6>&nbsp;</h6>
     </b-col>
     <b-col>
-      <b-row>
-        <b-col>
-          <notice
-          v-if="inclWarning"
-                class="danger mb-4"
-                :borderClass="'red'"
-                message="<p>Giving false or misleading information is a serious offence. A person who:
-                              <ul>
-                                  <li>knowingly or recklessly makes a false or misleading statement in an application for
-                                      an entry permit; or</li>
-                                  <li>knowingly gives false or misleading information in an application for an entry
-                                      permit</li>
-                              </ul>
-                              is guilty of an offence, the punishment for which is imprisonment for up to 12 months if
-                                  the statement is made or information is provided knowingly, or up to 6 months if the
-                                  statement is made recklessly - see Part 7.4, s.136 and s.137.1 of the Criminal Code.</p>"
-              ></notice>
-        </b-col>
-      </b-row>
+     
       <b-row>
         
         <b-col :cols="indented ? 9 : 6">
           
           <b-form-group label="Signature (type your name)">
-            <b-form-input v-model="signature.name"> </b-form-input>
+            <b-form-input v-model="signature.name" @click="onSignatureFocus"> </b-form-input>
           </b-form-group>
         </b-col>
         <b-col cols="12">
@@ -56,6 +38,38 @@
     </b-col>
   </b-row>
     
+  <b-modal :id="`warning`" title="BootstrapVue">
+        <template #modal-header="{ close }" >
+          <h6>Please note</h6>
+          <!-- Emulate built in modal header close button action -->
+          <i @click="close()" v-b-tooltip.hover title="Close" variant="danger" class="bi bi-x fs-3 removeIcon"></i>
+        </template>
+
+        <template #modal-footer="{ hide }">
+      
+      <b-button size="sm" variant="primary" @mouseenter="hide('forget')">
+        I Understand
+      </b-button>
+    </template>
+        <b-row>
+          <b-col>
+           
+         
+               <p>Giving false or misleading information is a serious offence. A person who:
+                              <ul>
+                                  <li>knowingly or recklessly makes a false or misleading statement in an application for
+                                      an entry permit; or</li>
+                                  <li>knowingly gives false or misleading information in an application for an entry
+                                      permit</li>
+                              </ul>
+                              is guilty of an offence, the punishment for which is imprisonment for up to 12 months if
+                                  the statement is made or information is provided knowingly, or up to 6 months if the
+                                  statement is made recklessly - see Part 7.4, s.136 and s.137.1 of the Criminal Code.</p>
+          </b-col>
+        </b-row>
+
+       
+      </b-modal>
 </div>
 </template>
 
@@ -65,7 +79,7 @@ import Notice from "../../common/notice.vue";
 export default {
   name: "f42Signature",
   components: {
-    Notice
+    Notice,
   },
   props: {
     form: {
@@ -84,13 +98,14 @@ export default {
       type: Boolean,
       default: false,
     },
-    inclWarning : {
+    inclWarning: {
       type: Boolean,
       default: false,
-    }
+    },
   },
   data() {
     return {
+      openedWarning: false,
       boolOptions: [
         { text: "Yes", value: true },
         { text: "No", value: false },
@@ -135,7 +150,14 @@ export default {
       return this.form.repType === "self" ? "have you" : "has the Applicant";
     },
   },
-  methods: {},
+  methods: {
+    onSignatureFocus() {
+      if (this.inclWarning && !this.openedWarning) {
+        this.$bvModal.show("warning");
+        this.openedWarning = true
+      }
+    },
+  },
 };
 </script>
 
